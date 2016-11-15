846f9ea4c0638d333d2868dd72dabf51bb0ebada
Arturo Rivera Flores <you@example.com>
2016-11-14 19:31:03 -0600

    da

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
   $tiempo = $_GET['time'];
-  $output = passthru("python water.py $tiempo");
+  $command = escapeshellcmd('./water.py ' . $tiempo);
+  $output = shell_exec($command);
   echo $output;
   echo "La planta se regara " . $tiempo . " segundos";
 ?>
